ac71708a20f9422c18f0b2d60280c8d5b71ff940
Hadwin Jiang <hadwinjiang@outlook.com>
2017-03-29 22:25:05 +0800

    Initial weak number

---
+++ CodeWars/WeakNumbers.Tests/myjinxin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeakNumbers.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class myjinxin
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new Kata();
+
+            Assert.AreEqual(new int[] { 2, 2 }, kata.WeakNumbers(9));
+
+            Assert.AreEqual(new int[] { 0, 1 }, kata.WeakNumbers(1));
+
+            Assert.AreEqual(new int[] { 0, 2 }, kata.WeakNumbers(2));
+
+            Assert.AreEqual(new int[] { 2, 1 }, kata.WeakNumbers(7));
+
+            Assert.AreEqual(new int[] { 403, 1 }, kata.WeakNumbers(500));
+
+            Assert.AreEqual(new int[] { 0, 4 }, kata.WeakNumbers(4));
+
+
+        }
+    }
+}
---
+++ CodeWars/WeakNumbers/myjinxin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeakNumbers
+{
+    public class Kata
+    {
+        private Tuple<int, int>[] weaknessRecord;
+
+        public Kata()
+        {
+            weaknessRecord = new Tuple<int, int>[1001];
+            weaknessRecord[1]= Tuple.Create(1,0);
+            for (int i = 2; i < 1001; i++)
+            {
+                int dividerNumber = CalculateDividerNumber(i);
+                int weaknessNumber = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (dividerNumber < weaknessRecord[j].Item1)
+                    {
+                        weaknessNumber++;
+                    }
+                }
+                weaknessRecord[i] = Tuple.Create(dividerNumber, weaknessNumber);
+            }
+
+        }
+
+        private int CalculateDividerNumber(int inputNumber)
+        {
+            int resultNumber = 1;
+            for (int i = 1; i < inputNumber; i++)
+            {
+                if ((inputNumber % i) == 0)
+                {
+                    resultNumber++;
+                }
+            }
+            return resultNumber;
+        }
+
+        public int[] WeakNumbers(int n)
+        {
+            //coding and coding..
+
+            int weaknessNumber = weaknessRecord[n].Item2;
+            int numbersOfWeaknessNumber = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (weaknessRecord[i].Item2 == weaknessNumber)
+                {
+                    numbersOfWeaknessNumber++;
+                }
+            }
+
+            return new int[]{ weaknessNumber, numbersOfWeaknessNumber};
+        }
+    }
+}
